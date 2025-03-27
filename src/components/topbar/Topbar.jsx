e77adc2ddab3725f33fb7cc3a49365579519f942
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-03-27 13:40:09 +0000

    add some update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./topbar.css";
-import {Search} from "@mui/icons-material";
+import { Search} from "@material-ui/icons";
 
 export default function Topbar() {
   return (
@@ -9,21 +9,23 @@ export default function Topbar() {
         <span className="logo">lamasocial</span>
       </div>
       <div className="topbarCenter">
-       <div className="searchbar">
-         <Search />
-         <input type="text" className="searchIput"  placeholder="Search..."/>
-       </div>
+        <div className="searchbar">
+          <Search />
+          <input className="searchIput" placeholder="Search..." />
+        </div>
       </div>
       <div className="topbarRight">
         <div className="topbarLink">
-          <span className="topbarLink">Home Page</span>
-          <span className="topbarLink">Home Page</span>
+          <span className="topbarLink">HomePage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcon">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
         </div>
       </div>
-      
-      
     </div>
-  )
+  );
 }
-
-
